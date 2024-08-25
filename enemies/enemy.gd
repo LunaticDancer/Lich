95f522eaabd8bd378c94a57330d74934d27d7c7c
lunaticdancergames@gmail.com <lunaticdancergames@gmail.com>
2024-08-25 11:28:02 +0200

    enemies drop ability pick ups

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,8 +4,11 @@ class_name Enemy
 @export var renderer : AnimatedSprite2D
 @export var hp = 1
 @export var points_value = 100
-@export var blood_drop : PackedScene
+var blood_drop = preload("res://enemies/blood_vfx/blood_drop.tscn")
 @export var blood_per_hit = 3
+var ability_pick_up_scene = preload("res://abilities/ability_pick_up.tscn")
+@export var ability_pool : Array[PackedScene]
+@export var ability_drop_chance = 0		# percent
 var invulnerable = false
 
 func bleed(direction):
@@ -15,11 +18,19 @@ func bleed(direction):
 		drop.global_position = global_position
 		add_sibling(drop)
 
+func handle_ability_drop():
+	if randf_range(1, 100) <= ability_drop_chance:
+		var drop = ability_pick_up_scene.instantiate()
+		drop.initiate(ability_pool.pick_random())
+		drop.global_position = global_position
+		add_sibling(drop)
+
 func receive_damage(amount : int):
 	if invulnerable:
 		return
 	
 	hp -= amount
 	if hp <= 0:
+		handle_ability_drop()
 		get_tree().get_first_node_in_group("GameController").enemy_killed(points_value)
 		renderer.play("death")
