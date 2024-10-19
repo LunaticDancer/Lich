2be32f940790199fd95cc22c8719ba333bbbf634
lunaticdancergames@gmail.com <lunaticdancergames@gmail.com>
2024-10-19 14:30:54 +0200

    healing system

--- core/player.gd
+++ core/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 const SPEED = 400.0
 const DASH_SPEED = 1600.0
 
-@export var basicAttackPrefab : PackedScene
+var basicAttackPrefab = preload("res://core/player_swing.tscn")
 var game_controller
 var is_dashing = false
+var is_healing = false
 
 func _ready():
 	get_tree().get_first_node_in_group("Camera").prepare_for_gameplay(self)
@@ -15,8 +16,9 @@ func _process(delta):
 	$AnimatedSprite2D.scale = Vector2(1 if (get_local_mouse_position().x > 0) else -1, 1)
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if is_healing:
+		return
+	
 	var xdirection = Input.get_axis("left", "right")
 	var ydirection = Input.get_axis("up", "down")
 	var direction = Vector2(xdirection, ydirection).normalized()
@@ -28,14 +30,29 @@ func _unhandled_input(event):
 		attack()
 	if event.is_action_pressed("dodge"):
 		dash()
+	if event.is_action_pressed("restore"):
+		if game_controller.mana < 100:
+			return
+		$AnimatedSprite2D.play("healing")
+		is_healing = true
+		$GPUParticles2D.emitting = true
+	if event.is_action_released("restore"):
+		$AnimatedSprite2D.play("default")
+		is_healing = false
+		$GPUParticles2D.emitting = false
 
 func attack():
+	if is_healing:
+		return
+		
 	var attack = basicAttackPrefab.instantiate()
 	attack.global_position = global_position + get_local_mouse_position().normalized() * 8
 	attack.set_direction( get_local_mouse_position().angle() )
 	get_parent().add_child(attack)
 
 func dash():
+	if is_healing:
+		return
 	if game_controller.dashes < 1:
 		return
 	
@@ -48,3 +65,11 @@ func receive_damage(amount : int):
 
 func _on_dash_timeout_timeout():
 	is_dashing = false
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "healing":
+		$AnimatedSprite2D.play("default")
+		is_healing = false
+		game_controller.receive_mana_damage(100)
+		get_tree().get_first_node_in_group("PlayerWeakpoint").heal()
+		$GPUParticles2D.emitting = false
